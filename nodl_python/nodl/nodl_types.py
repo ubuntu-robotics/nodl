fd8a77ad2347dacad2eba422d3e6cf442c648e57
Ted Kern <ted.kern@canonical.com>
2020-01-29 12:39:26 -0800

    linter tests and make api use keyword-only args
    Signed-off-by: Ted Kern <ted.kern@canonical.com>

--- nodl_python/nodl/nodl_types.py
+++ nodl_python/nodl/nodl_types.py
@@ -11,13 +11,16 @@
 # If not, see <http://www.gnu.org/licenses/>.
 
 
+from typing import List, Optional
+
 from rclpy.qos import QoSProfile, QoSPresetProfiles
 
 
-class nodl_action():
+class NoDLAction:
     """Data structure for action entries in NoDL."""
 
     def __init__(self,
+                 *,
                  name: str,
                  msg_type: str,
                  server: bool = False,
@@ -31,35 +34,63 @@ class nodl_action():
         self.qos = qos
 
 
-class nodl_service():
+class NoDLParameter:
+    def __init__(self,
+                 *,
+                 name: str,
+                 param_type: str):
+        self.name = name
+        self.type = param_type
+
+
+class NoDLService:
     """Data structure for service entries in NoDL."""
 
     def __init__(self,
+                 *,
                  name: str,
-                 msg_type: str,
+                 service: str,
                  server: bool = False,
                  client: bool = False,
                  qos: QoSProfile = QoSPresetProfiles.SERVICES_DEFAULT):
         self.name = name
-        self.type = msg_type
+        self.type = service
         self.server = server
         self.client = client
 
         self.qos = qos
 
 
-class nodl_topic():
+class NoDLTopic:
     """Data structure for topic entries in NoDL."""
 
     def __init__(self,
+                 *,
                  name: str,
-                 msg_type: str,
+                 message: str,
                  server: bool = False,
                  client: bool = False,
-                 qos: str = QoSPresetProfiles.SYSTEM_DEFAULT):
+                 qos: QoSProfile = QoSPresetProfiles.SYSTEM_DEFAULT):
         self.name = name
-        self.type = msg_type
+        self.type = message
         self.server = server
         self.client = client
 
         self.qos = qos
+
+
+class NoDLNode:
+    """"""
+
+    def __init__(self, *,
+                 name: str,
+                 actions: Optional[List[NoDLAction]] = None,
+                 parameters: Optional[List[NoDLParameter]] = None,
+                 services: Optional[List[NoDLService]] = None,
+                 topics: Optional[List[NoDLTopic]] = None):
+        self.name = name
+
+        self.actions = actions if actions else []
+        self.parameters = parameters if parameters else []
+        self.services = services if services else []
+        self.topics = topics if topics else []
