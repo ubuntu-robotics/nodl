b2ef44ccd14c9015e067c41903be3e3abbbc8004
Ted Kern <ted.kern@canonical.com>
2020-02-10 19:03:15 +0000

    fix changing copyright notice accidentally
    Signed-off-by: Ted Kern <ted.kern@canonical.com>

--- nodl_python/test/test_pep257.py
+++ nodl_python/test/test_pep257.py
@@ -1,14 +1,14 @@
 # Copyright 2020 Canonical Ltd.
 #
 # This program is free software: you can redistribute it and/or modify it under the terms of the
-# GNU General Public License version 3, as published by the Free Software Foundation.
+# GNU Limited General Public License version 3, as published by the Free Software Foundation.
 #
 # This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
 # without even the implied warranties of MERCHANTABILITY, SATISFACTORY QUALITY, or
-# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Limited General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License along with this program.
-# If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU Limited General Public License along with
+# this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 from ament_pep257.main import main
