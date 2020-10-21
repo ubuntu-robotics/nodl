1d64633af44cf0fd7743cc4fbddbc2c74144ae8a
Xiyang Yeh <xiyangyeh@gmail.com>
2020-10-20 21:47:44 -0700

    updates

---
+++ test_package/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'test_package'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='xyyeh',
+    maintainer_email='xiyangyeh@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'test_node = test_package.test_node:main'
+        ],
+    },
+)
---
+++ test_package/test_package/test_node.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from test_package.')
+
+
+if __name__ == '__main__':
+    main()
